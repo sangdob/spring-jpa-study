f54641cd76539a1d35ede4640cd2d69c192937d8
sangdob <51118110+sangdob@users.noreply.github.com>
2022-10-27 23:32:16 +0900

    JPA STUDY
    jpql test
     - fetch join test

---
+++ jpa/src/main/java/com/jpa/jpql/FecthJoinTest.java
@@ -0,0 +1,69 @@
+package com.jpa.jpql;
+
+import com.jpa.oneway.Member;
+import com.jpa.oneway.Team;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+@Slf4j
+public class FecthJoinTest {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("jpaStudy");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("name1");
+            entityManager.persist(team);
+
+            Team team1 = new Team();
+            team1.setName("팀2");
+            entityManager.persist(team1);
+
+
+            Member member = new Member();
+            member.setUserName("회원1");
+            member.setTeam(team);
+            entityManager.persist(member);
+
+
+            Member member1 = new Member();
+            member.setUserName("회원2");
+            entityManager.persist(member1);
+            member.setTeam(team1);
+            entityManager.persist(member1);
+
+            Member member2 = new Member();
+            member.setUserName("회원3");
+            member.setTeam(team);
+            entityManager.persist(member2);
+
+            entityManager.flush();
+            entityManager.clear();
+
+            String query = "SELECT m from member m join fetch t.members";
+
+            List<Member> findAllMember = entityManager.createQuery(query, Member.class).getResultList();
+
+            for ( Member m : findAllMember) {
+                log.info("member = {}", m.toString());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.getStackTrace();
+        } finally {
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+    }
+}
