aa7ee9cf1c5f34813b4beeaa295fb4f083b3e72b
sangdob <51118110+sangdob@users.noreply.github.com>
2022-11-09 23:37:29 +0900

    jpa shop test
    item service logic add

---
+++ shop/src/main/java/jpa/shop/repository/ItemRepository.java
@@ -0,0 +1,33 @@
+package jpa.shop.repository;
+
+import jpa.shop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+    private final EntityManager entityManager;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            entityManager.persist(item);
+        } else {
+            entityManager.merge(item);
+        }
+        entityManager.flush();
+        entityManager.clear();
+    }
+
+    public Item findByItem(Long id) {
+        return entityManager.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return entityManager.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
---
+++ shop/src/main/java/jpa/shop/service/ItemService.java
@@ -0,0 +1,32 @@
+package jpa.shop.service;
+
+import jpa.shop.domain.item.Item;
+import jpa.shop.repository.ItemRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Getter
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findByItem(Long id) {
+        return itemRepository.findByItem(id);
+    }
+}
