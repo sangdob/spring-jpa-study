a646a66473f53e607a2621471eda7940505a8759
sangdob <51118110+sangdob@users.noreply.github.com>
2022-11-24 23:49:59 +0900

    jpa shop test
    post new member add

--- shop/src/main/java/jpa/shop/controller/MemberController.java
+++ shop/src/main/java/jpa/shop/controller/MemberController.java
@@ -1,19 +1,37 @@
 package jpa.shop.controller;
 
+import jpa.shop.domain.Address;
+import jpa.shop.domain.Member;
 import jpa.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/members")
 public class MemberController {
     private final MemberService memberService;
 
-    @GetMapping("/members/new")
+    @GetMapping("/new")
     public String createForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "members/createMemberForm";
+    }
+
+    @PostMapping("/new")
+    public String create(MemberForm memberForm) {
+        Address address = new Address(memberForm.getCity(), memberForm.getStreet(), memberForm.getZipCode());
+
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
--- shop/src/main/java/jpa/shop/controller/MemberForm.java
+++ shop/src/main/java/jpa/shop/controller/MemberForm.java
@@ -1,4 +1,13 @@
 package jpa.shop.controller;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
 public class MemberForm {
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipCode;
 }
