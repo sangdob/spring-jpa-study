8234799bba68d343aa8dbfd5f08863d75787150d
sangdob <51118110+sangdob@users.noreply.github.com>
2022-11-27 22:51:16 +0900

    jpa shop test
    item controller post get add

---
+++ shop/src/main/java/jpa/shop/controller/BookForm.java
@@ -0,0 +1,18 @@
+package jpa.shop.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookForm {
+
+    private Long id;
+
+    private String name;
+    private Long price;
+    private Long stockQuantity;
+
+    private String author;
+    private String isbn;
+}
