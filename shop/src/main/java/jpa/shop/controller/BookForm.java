8473774511bccdd9b2ab39dafd96ad625a129606
sangdob <51118110+sangdob@users.noreply.github.com>
2022-12-01 19:51:21 +0900

    jpa shop test
    post items add

--- shop/src/main/java/jpa/shop/controller/BookForm.java
+++ shop/src/main/java/jpa/shop/controller/BookForm.java
@@ -8,11 +8,9 @@ import lombok.Setter;
 public class BookForm {
 
     private Long id;
-
     private String name;
     private Long price;
     private Long stockQuantity;
-
     private String author;
     private String isbn;
 }
--- shop/src/main/java/jpa/shop/controller/ItemController.java
+++ shop/src/main/java/jpa/shop/controller/ItemController.java
@@ -6,10 +6,7 @@ import jpa.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -58,5 +55,20 @@ public class ItemController {
 
         model.addAttribute("form", form);
         return "items/updateItemForm";
+    }
+
+    @PostMapping("/{items}/edit")
+    public String updateItem(@PathVariable String itemId, @ModelAttribute("form") BookForm form) {
+        Book book = new Book();
+
+        book.setId(form.getId());
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/items";
     }
 }
