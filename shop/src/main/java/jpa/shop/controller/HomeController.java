baafee0091d8c572b0fa1383794e4c1446803b19
sangdob <51118110+sangdob@users.noreply.github.com>
2022-12-12 23:46:08 +0900

    jpa shop test
    main route test

--- shop/src/main/java/jpa/shop/controller/HomeController.java
+++ shop/src/main/java/jpa/shop/controller/HomeController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequestMapping
 public class HomeController {
 
     @RequestMapping("/")
     public String home() {
+        log.info("homeController");
         return "home";
     }
 }
