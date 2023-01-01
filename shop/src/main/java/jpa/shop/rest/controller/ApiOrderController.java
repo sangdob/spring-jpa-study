5205b7e495fbcf452f830dec44ba76a4054bcc33
sangdob <51118110+sangdob@users.noreply.github.com>
2023-01-01 23:57:21 +0900

    jpa shop test
    order api add

---
+++ shop/src/main/java/jpa/shop/rest/controller/ApiOrderController.java
@@ -0,0 +1,30 @@
+package jpa.shop.rest.controller;
+
+import jpa.shop.domain.Order;
+import jpa.shop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * XToOne
+ * order
+ * order -> member
+ * order ->delivery
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ApiOrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping("/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> orders = orderService.findOrders();
+        return orders;
+    }
+}
